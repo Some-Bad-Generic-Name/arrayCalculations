72dfae89c8ddd9842ac5ab5cf8775a7dff40d9df
Some-Bad-Generic-Name <someordinaryname@gmail.com>
2022-12-20 16:15:16 -0500

    added the basic functions

--- main.c
+++ main.c
@@ -6,14 +6,44 @@
 int main(){
 	srand(time(NULL));
 	
-	int arraySize = 10;
+	int arraySize = 5;
 	int array[arraySize];
+
+	int max = 0;
+	int min = 0;
+	
+	double sum = 0;
+	double average = 0.0;
+
+	arraySize -= 1; // to account for the fact the arrays start at zero
 
 	for(int i = 0; i <= arraySize; i++)
 	{
 		array[i] = rand();
 		printf("%d\n", array[i]);
 	}
+
+	for(int i = 0; i <= arraySize; i++)
+	{
+		if(max < array[i]) max = array[i];
+		else continue;
+	}
+
+	min = max;
+
+	for(int i = 0; i <= arraySize; i++)
+	{
+		if(min > array[i]) min = array[i];
+		else continue;
+	}
+
+	for(int i = 0; i <= arraySize; i++)
+	{
+		sum = sum += array[i];
+	}
+	average = sum/(arraySize + 1);
+
+	printf("\n%d\n%d\n%lf\n%lld", max, min, average, sum);
 
 	return 0;
 }
